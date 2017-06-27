11b9a792aa0fb5e6626379cbb79d2e432b300361
Philipp Gorczak <p.gorczak@gmail.com>
2017-06-27 17:16:30 +0200

    Moar comments.

--- netz.py
+++ netz.py
@@ -17,8 +17,9 @@ def compose_project():
     from compose.cli.command import project_from_options
     from docopt import docopt
 
-    # Forward all args after "--"
+    # Forward all args after "--".
     forwards = list(islice(dropwhile(lambda x: x != '--', sys.argv), 1, None))
+    # Parse into a Docker Compose project.
     compose_opts = docopt(
         inspect.getdoc(compose_main.TopLevelCommand),
         argv=forwards,
@@ -27,21 +28,27 @@ def compose_project():
 
 
 def project_containers(project):
+    """ Get all containers that belong to a Compose project. """
     return tuple(c for c in client.containers.list() if
                  c.labels.get('com.docker.compose.project', None) == project)
 
 
 def project_networks(project):
+    """ Get all networks that belong to a Compose project. """
     return tuple(
         n for n in client.networks.list() if
         n.attrs['Labels'].get('com.docker.compose.project', None) == project)
 
 
 def interface_in_net(c, n):
-    """ Get interface of container c attached to net n. """
+    """ Get interface at which container c is attached to network n. """
+    # Container MAC address in network.
     mac = n.attrs['Containers'][c.id]['MacAddress']
+    # List of all network interfaces on the container.
     devices = c.exec_run(['ls', '-1', '/sys/class/net']).decode()
+    # List of their MAC addresses.
     addrs = c.exec_run(['sh', '-c', 'cat /sys/class/net/*/address']).decode()
+    # Build MAC->interface dict and get the interface we're looking for.
     addr_to_dev = dict(zip(addrs.splitlines(), devices.splitlines()))
     return addr_to_dev[mac]
 
@@ -62,23 +69,30 @@ if __name__ == '__main__':
     name = compose_project().name
     log.info('Using project name "{}".'.format(name))
 
+    # name->instance for all networks in the Compose project.
     nets = {n.attrs['Labels']['com.docker.compose.network']: n for n in
             project_networks(name)}
+    # Ditto for containers.
     cons = {c.labels['com.docker.compose.service']: c for c in
             project_containers(name)}
 
+    # Iterate top level (network names).
     for net_name, net_cfg in config.items():
+        # Get network instance by name.
         try:
             net = nets[net_name]
         except KeyError as e:
             log.error('Network "{}" not found.'.format(net_name))
             exit(1)
+        # Iterate second level (container name -> netem options)
         for con_name, cmd in net_cfg.items():
+            # Get container instance by name.
             try:
                 con = cons[con_name]
             except KeyError as e:
                 log.error('Container "{}" not found.'.format(net_name))
                 exit(1)
+            # Execute the command on the container.
             try:
                 replace_netem(con, net, cmd)
             except RuntimeError as e:
